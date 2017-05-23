195afe9b918f9a72bf80d501d8ffe658c93a75a3
SefikaBoz <sefikaboz12@gmail.com>
2017-05-23 18:40:58 +0300

    Create Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,37 @@
+# 23-05-2017---8
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatesAndInterface
+{
+    class Animal { }
+    class Cat : Animal { }
+    class Dog : Animal { }
+    class Pets : IEnumerable<Cat>, IEnumerable<Dog>
+    {
+        IEnumerator<Cat> IEnumerable<Cat>.GetEnumerator()
+        {
+            Console.WriteLine("Cat chosen");
+            return null;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // Some code.
+            return null;
+        }
+        IEnumerator<Dog> IEnumerable<Dog>.GetEnumerator()
+        {
+            Console.WriteLine("Dog chosen");
+            // Some code.
+            return null;
+        }
+    }
+   
+}
+
+
