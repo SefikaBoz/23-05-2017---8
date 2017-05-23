5614dba92b3859e028af872e4a884a9bb8fe57b6
SefikaBoz <sefikaboz12@gmail.com>
2017-05-23 18:41:17 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+# 23-05-2017---8
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatesAndInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Dog> pets = new Pets();
+            pets.GetEnumerator();
+
+            IEnumerable<Cat> pet = new Pets();
+            pet.GetEnumerator();
+
+            Console.ReadLine();
+        }
+    }
+}
